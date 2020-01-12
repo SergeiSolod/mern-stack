c193940ba65fc2a2b6314ec0427bd65d283082bd
SergeySolod <48413787+SergeySolod@users.noreply.github.com>
2020-01-11 20:43:17 -0800

    Added useHttp hook

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useHttp} from "../hooks/http.hook";
 
 export const AuthPage = () => {
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
+    const changeHandler = (event) => {
+        setForm({...form, [event.target.name]: event.target / value})
+    }
+
     return (
         <div className='row'>
             <div className="col s6 offset-s3">
@@ -11,7 +19,16 @@ export const AuthPage = () => {
                     <div className="card-content white-text">
                         <span className="card-title">Авторизация</span>
                         <div>
-
+                            <div className="input-field">
+                                <input placeholder="Введите email" id="email" type="text" name="email"
+                                       className="yellow-input" onChange={changeHandler}/>
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input placeholder="Введите пароль" id="password" type="password" name="password"
+                                       className="yellow-input" onChange={changeHandler}/>
+                                <label htmlFor="password">Пароль</label>
+                            </div>
                         </div>
                     </div>
                     <div className="card-action">
