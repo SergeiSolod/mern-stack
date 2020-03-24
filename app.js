340771c175d6c017c001b6ca543f54dc8cda5e29
SergeySolod <solodsergey287@gmail.com>
2020-03-24 23:22:30 +0300

    mongodb

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const PORT = config.get('port') || 5000
 async function start() {
     try {
         await  mongoose.connect(config.get('mongoUri'), {
-
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
         })
     } catch (e) {
         console.log('Server error', e.message)
@@ -18,5 +20,3 @@ async function start() {
 }
 
 start()
-
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
