e41118e1c442b1ce8f70d870b97e595535a1067d
SergeySolod <solodsergey287@gmail.com>
2020-03-24 23:45:53 +0300

    Add register

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,25 @@
 const {Router} = require('express')
+
 const router = Router()
+
+// /api/auth/register
+router.post('/register', async(req, res) => {
+    try {
+        const {email, password} = req.body
+
+    } catch (e) {
+        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'})
+    }
+
+})
+
+// /api/auth/login
+router.post('/login', async(req, res) => {
+    try {
+
+    } catch (e) {
+
+    }
+})
 
 module.exports = router
