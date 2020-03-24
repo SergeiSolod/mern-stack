0ea2de1def657c8afaa818a45e350566d2b82261
SergeySolod <solodsergey287@gmail.com>
2020-03-24 23:04:42 +0300

    Before add mongodb

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.listen(5000, () => console.log('App has been started...'))
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await  mongoose.connect(config.get('mongoUri'), {
+
+        })
+    } catch (e) {
+        console.log('Server error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
