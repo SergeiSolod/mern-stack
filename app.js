21d31c881595645df3d7e2adfb4300e3b00f1277
SergeySolod <solodsergey287@gmail.com>
2020-04-16 11:51:27 +0300

    New commit

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
 
+const app = express()
 const PORT = config.get('port') || 5000
 
-const app = express()
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'),{
+
+        })
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
 
 app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
