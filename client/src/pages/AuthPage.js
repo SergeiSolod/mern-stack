e00e890b9a44cc3c35da0e275b7b026e40fedffe
SergeySolod <solodsergey287@gmail.com>
2020-04-16 18:10:42 +0300

    Add input

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 const AuthPage = props => {
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className='row'>
+            <div className="col s6 offset-s3">
+                <h1>Сократи ссылку</h1>
+                <div className="card blue darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Авторизация</span>
+                        <div>
+                            <div className="input-field col s6">
+                                <input placeholder="Placeholder" id="first_name" type="text" className="validate">
+                                    <label htmlFor="first_name">First Name</label>
+                            </div>
+                            <div className="input-field col s6">
+                                <input placeholder="Placeholder" id="first_name" type="text" className="validate">
+                                    <label htmlFor="first_name">First Name</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className='btn yellow darken-4' style={{marginRight: 10}}>Войти</button>
+                        <button className='btn grey lighten-1 black-text'>Регистрация</button>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+)
 }
 
 export default AuthPage
