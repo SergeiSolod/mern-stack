f3d15681dc3aed4f84100dd8f0663efbc06474d7
SergeySolod <solodsergey287@gmail.com>
2020-04-16 11:45:04 +0300

    add mongoose

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('asd')
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const PORT = config.get('port') || 5000
+
+const app = express()
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
