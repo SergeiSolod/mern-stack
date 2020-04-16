4b3a5c06dd6a670e7262013f3354c9791efc8188
SergeySolod <solodsergey287@gmail.com>
2020-04-16 12:02:59 +0300

    add file

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const PORT = config.get('port') || 5000
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUri'),{
-
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
         })
     } catch (e) {
         console.log('Server Error', e.message)
