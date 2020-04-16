aae7c880880eef31602ab6ffebe71a77a32264d4
SergeySolod <solodsergey287@gmail.com>
2020-04-16 13:40:39 +0300

    New commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,8 @@ const router = Router()
 
 router.post('/register', async (req, res) => {
     try {
-
+        const {email, password} = req.body
+        
     } catch (e) {
         res.status(500),json({message: 'Что-то пошло не так, попробуйте снова'})
     }
